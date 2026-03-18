743f77fa3873fea386aeb49a3e6796e47ac5eb77
Shiyu <shiyuwang781@gmail.com>
2026-03-18 21:08:18 +0800

    0318post

--- app/posts/some-fatal-warnings-of-thermodymamics/page.tsx
+++ app/posts/some-fatal-warnings-of-thermodymamics/page.tsx
@@ -4,7 +4,7 @@ import Math from '@/components/Math';
 export default function Post() {
   return (
     <PostLayout
-      title="Some Fatal Warnings of Thermodynamics"
+      title="Some Fatal Warnings about Thermodynamics"
       date="2026-03-17 20:00:00 +0800"
       categories={["Physics", "Thermodynamics"]}
       isHighlight={true}
@@ -33,7 +33,50 @@ export default function Post() {
       <p>In fact, the reversible process is a special case of the general process, and the relation between Enthalpy and heat is more complicated than that. So, we should be careful when we use this relation, and we should always check the conditions before applying it.</p>
 
       <h3>Fatal Warning 2: Gibbs and Helmholtz Free Energies.</h3>
-      <p>leave them to tomorrow.</p>
+      <p>The core idea of Gibbs and Helmholtz free energy is to 
+        consider the system and environment as a isolated system, 
+        so that the criteria of spontaneous process is the change of 
+        the total entropy of the isolated system.
+      </p>
+      <p>However, during the deduction of deriving free energies, some 
+        assumptions was involved, which makes the free energy criteria
+        have some limitations, which is often neglected by textbooks and courses. 
+      </p>
+
+      <p>Consider the environment is big enough so that its temperature is constant.
+        Then, the change of the total entropy of the isolated system is: </p>
+      <Math math="dS_{iso} = dS_{sys} + dS_{sur} \geq dS_{sys} - \frac{d Q}{T_{sur}}" block />
+      <h4>isochoric process</h4>
+      <p>For an isochoric process, we have: <Math math='dQ = dU'/>so that:</p>
+      <Math math="dS_{iso} \geq dS_{sys} - \frac{dU}{T_{sur}} = \frac{1}{T_{sur}}(T_{sur}dS_{sys} - dU)" block />
+      <p> Since <Math math="T_{sur} > 0"/>, we can deduce <Math math="dS_{iso} \geq 0"/> from <Math math='T_{sur}dS_{sys}-dU \geq 0'/></p>
+      <p> If <strong>system temperature and surrounding temperature are identical</strong>, we have:</p>
+      <Math math='T_{sur}dS_{sys}-dU \geq 0 \iff T_{sys}dS_{sys}-dU \geq 0' block />
+      <p>So, we can define a <strong>state function whose decrease can indicate the increase of total entropy</strong>: </p>
+      <Math math='A=U-T_{sys}S_{sys}' block/>
+      <Math math='dA=dU-T_{sys}dS_{sys}-S_{sys}dT_{sys}' block/>
+      <p>For a isothermo process, <Math math='dA=dU-T_{sys}dS_{sys}'/> and the negative <Math math='dA'/> indicates a positive <Math math='dS_{iso}'/></p>
+      <p>As you saw, a lot of assumptions was introduced into the deduction.
+        So, the criteria of Helmholtz free energy is only valid for isothermal process, and <strong>the system temperature must be identical to the surrounding temperature.</strong>
+      </p>
+
+      <h4>isobaric process</h4>
+      <p>For an isobaric process<strong>where system pressure equals surrounding pressure</strong>, we have: <Math math='dQ = dH'/>so that:</p>
+      <Math math="dS_{iso} \geq dS_{sys} - \frac{dH}{T_{sur}}" block />
+      <Math math="dS_{iso} \geq \frac{1}{T_{sur}}(T_{sur}dS_{sys} - dH)" block />
+        <p> If <strong>system temperature and surrounding temperature are identical</strong>, we have:</p>
+        <Math math='T_{sur}dS_{sys}-dH \geq 0 \iff T_{sys}dS_{sys}-dH \geq 0' block />
+        <p>So, we can define a <strong>state function whose decrease can indicate the increase of total entropy</strong>: </p>
+        <Math math='G=H-T_{sys}S_{sys}' block/>
+        <Math math='dG=dH-T_{sys}dS_{sys}-S_{sys}dT_{sys}' block/>
+        <p>For a isothermo process, <Math math='dG=dH-T_{sys}dS_{sys}'/> and the negative <Math math='dG'/> indicates a positive <Math math='dS_{iso}'/></p>
+        <p>As you saw, a lot of assumptions was introduced into the deduction.
+          So, the criteria of Gibbs free energy is only valid for isothermal process, and <strong>the system temperature must be identical to the surrounding temperature.</strong>
+        </p>
+
+      <p>In conclusion, the criteria of free energies are only valid for isothermal processes, and <strong>the system temperature must be identical to the surrounding temperature.</strong> So, we should be careful when we use these criteria, and we should always check the conditions before applying them.</p>
+    <h3>Others</h3>
+    <p>Currently, I'm reviewing the concepts of thermodynamics and their applications. So I will update my new findings here.</p>
     </PostLayout>
   );
 }
